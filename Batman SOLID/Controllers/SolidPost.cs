951e6542d223726d8cbabdc41671b44357e7c059
kaijengwong <kaijengwong@micron.com>
2023-06-04 15:19:06 +0800

    Add Open Closed

--- Batman SOLID/Controllers/SolidPost.cs
+++ Batman SOLID/Controllers/SolidPost.cs
@@ -44,9 +44,21 @@ namespace Batman_SOLID.Controllers
                                 res + "3: " + StudentResult();
 
                     break;
+
                 case "o":
                     solid = "open-closed principle";
+
+                    var devCalculations = new List<BaseSalaryCalculator>
+                    {
+                        new JuniorDevSalaryCalculator(new DeveloperReport {Id = 1, Name = "Dev1", Level = "Junior developer", HourlyRate = 20, WorkingHours = 100 }),
+                        new SeniorDevSalaryCalculator(new DeveloperReport {Id = 2, Name = "Dev2", Level = "Senior developer", HourlyRate = 30.5, WorkingHours = 120 }),
+                        new SeniorDevSalaryCalculator(new DeveloperReport {Id = 3, Name = "Dev3", Level = "Senior developer", HourlyRate = 30.5, WorkingHours = 150 })
+                    };
+                    var calculator = new SalaryCalculator(devCalculations);
+
+                    solidpost = "Sum of all the developer salaries: " + calculator.CalculateTotalSalaries();
                     break;
+
                 case "l":
                     solid = "Liskov substitution principle";
                     break;
@@ -62,6 +74,7 @@ namespace Batman_SOLID.Controllers
             return Ok(new {Principle = solid, SOLID = solidpost });
         }
 
+        //Single Responsibility
         private string StudentRegister()
         {
             return "Student Registered";
@@ -75,7 +88,52 @@ namespace Batman_SOLID.Controllers
         { 
             return "Send Email to Student";
             
-        } 
+        }
+
+        //Open Closed
+        public abstract class BaseSalaryCalculator
+        {
+            protected DeveloperReport DeveloperReport { get; private set; }
+            public BaseSalaryCalculator(DeveloperReport developerReport)
+            {
+                DeveloperReport = developerReport;
+            }
+            public abstract double CalculateSalary();
+        }
+        public class SalaryCalculator
+        {
+            private readonly IEnumerable<BaseSalaryCalculator> _developerCalculation;
+            public SalaryCalculator(IEnumerable<BaseSalaryCalculator> developerCalculation)
+            {
+                _developerCalculation = developerCalculation;
+            }
+            public double CalculateTotalSalaries()
+            {
+                double totalSalaries = 0D;
+                foreach (var devCalc in _developerCalculation)
+                {
+                    totalSalaries += devCalc.CalculateSalary();
+                }
+                return totalSalaries;
+            }
+        }
+
+        public class JuniorDevSalaryCalculator : BaseSalaryCalculator
+        {
+            public JuniorDevSalaryCalculator(DeveloperReport developerReport)
+                : base(developerReport)
+            {
+            }
+            public override double CalculateSalary() => DeveloperReport.HourlyRate * DeveloperReport.WorkingHours;
+        }
+        public class SeniorDevSalaryCalculator : BaseSalaryCalculator
+        {
+            public SeniorDevSalaryCalculator(DeveloperReport report)
+                : base(report)
+            {
+            }
+            public override double CalculateSalary() => DeveloperReport.HourlyRate * DeveloperReport.WorkingHours * 1.2;
+        }
     }
 
 }
--- Batman SOLID/SolidPost.cs
+++ Batman SOLID/SolidPost.cs
@@ -3,8 +3,16 @@ namespace Batman_SOLID
     public class SolidPost
     {
         public string type { get; set; }
-        public string animal { get; set; }
 
 
+    }
+
+    public class DeveloperReport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public int WorkingHours { get; set; }
+        public double HourlyRate { get; set; }
     }
 }

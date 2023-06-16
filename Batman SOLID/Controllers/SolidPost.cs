6358e775bc64ae4c9587a0d68f938a7e9d0ce005
kaijengwong <kaijengwong@micron.com>
2023-06-16 10:36:39 +0800

    Add Dependency Inversion

--- Batman SOLID/Controllers/SolidPost.cs
+++ Batman SOLID/Controllers/SolidPost.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Batman_SOLID.Controllers.SolidPostController;
 
 //The single-responsibility principle (SRP) is a computer - programming principle that states that every module, class or function in a computer program should have responsibility over a single part of that program's functionality, and it should encapsulate that part. All of that module, class or function's services should be narrowly aligned with that responsibility
 //https://en.wikipedia.org/wiki/Single-responsibility_principle
@@ -79,9 +80,9 @@ namespace Batman_SOLID.Controllers
                     break;
                 case "d":
                     solid = "dependency inversion principle";
-
-
-
+                    EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+                    Employee emp = employeeBusinessLogic.GetEmployeeDetails(1001);
+                    solidpost = "ID: " + emp.ID + ", Name: " + emp.Name + ", Department: " + emp.Department + ", Salary: " + emp.Salary;
                     break;
                 default:
                     break;
@@ -167,6 +168,55 @@ namespace Batman_SOLID.Controllers
             public string GetColor()
             {
                 return "Orange";
+            }
+        }
+
+        //Dependency Inversion
+        public class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Department { get; set; }
+            public int Salary { get; set; }
+        }
+        public interface IEmployeeDataAccessLogic
+        {
+            Employee GetEmployeeDetails(int id);
+            //Any Other Employee Related Method Declarations
+        }
+        public class EmployeeDataAccessLogic : IEmployeeDataAccessLogic
+        {
+            public Employee GetEmployeeDetails(int id)
+            {
+                //In real time get the employee details from database
+                //but here we have hard coded the employee details
+                Employee emp = new Employee()
+                {
+                    ID = id,
+                    Name = "Pranaya",
+                    Department = "IT",
+                    Salary = 10000
+                };
+                return emp;
+            }
+        }
+        public class DataAccessFactory
+        {
+            public static IEmployeeDataAccessLogic GetEmployeeDataAccessObj()
+            {
+                return new EmployeeDataAccessLogic();
+            }
+        }
+        public class EmployeeBusinessLogic
+        {
+            IEmployeeDataAccessLogic _IEmployeeDataAccessLogic;
+            public EmployeeBusinessLogic()
+            {
+                _IEmployeeDataAccessLogic = DataAccessFactory.GetEmployeeDataAccessObj();
+            }
+            public Employee GetEmployeeDetails(int id)
+            {
+                return _IEmployeeDataAccessLogic.GetEmployeeDetails(id);
             }
         }
     }
